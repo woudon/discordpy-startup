cbed4496af0bb0c193006e89e079c659445806a7
woudon <70763888+woudon@users.noreply.github.com>
2021-01-08 19:57:36 +0900

    jk alice

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,15 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/js':
+        await message.channel.send('jkじゃい')
 
 
 bot.run(token)
